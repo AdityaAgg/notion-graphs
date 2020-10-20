5d40dae6b249ca7cb6196795ce0bfa7001371c55
AdityaAgg <adityaaggarwalz200@gmail.com>
2020-10-20 04:10:11 -0700

    integration w/ new api

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,21 @@ import refreshIcon from './loop2.svg';
 import { mutate } from "swr";
 
 interface DataPoint {
-  value: number;
   id: number;
+  x: number;
+  y: number;
   size: number;
+  title: string;
 }
 
 const App: React.FC = () => {
   const svgRef = useRef<null | SVGSVGElement>(null);
-  let { data } = useSWR<DataPoint[]>(window.location.search,
+  const searchLocation = window.location.search;
+  let { data } = useSWR<DataPoint[]>(searchLocation,
     async (params) => {
-      const response = await fetch("https://pnw3gi739c.execute-api.us-east-1.amazonaws.com/production-stage/get_all_events" + params)
+      const response = await fetch("https://l175wxlpxi.execute-api.us-east-1.amazonaws.com/production/line_graph" + params)
       const jsonResponse = await response.json()
-      return jsonResponse["events"]
+      return jsonResponse["data_points"]
     }, { refreshInterval: 30000 }
   );
 
@@ -30,20 +33,42 @@ const App: React.FC = () => {
       return;
     }
 
+
+    if (!data) {
+      return;
+    }
+    data = data.filter(data_point => data_point.x != null && data_point.y != null && data_point.size != null);
+    let findMinAndMax = (data: any, property: string) => {
+      let postProcessMinMax = (min: any, max: any) => {
+        if (max === min) {
+          if (max === 0)
+            max += 1;
+          else
+            max += Math.abs(max) * 0.1;
+        }
+        return [min, max]
+      }
+      return postProcessMinMax(Math.min(...data.map((dataPoint: any) => dataPoint[property])),
+        Math.max(...data.map((dataPoint: any) => dataPoint[property])));
+    };
+
+    let [x_min, x_max] = findMinAndMax(data, "x");
+    let [y_min, y_max] = findMinAndMax(data, "y");
+
     let svg = select(svgRef.current);
     let width = svgRef.current.clientWidth - 50;
     let height = svgRef.current.clientHeight - 50;
     let xScale = scaleLinear()
-      .domain([0, 50]) // input
-      .range([25, width]); // output
+      .domain([x_min, x_max + Math.abs(x_max - x_min) * 0.1])
+      .range([25, width]);
 
     let yScale = scaleLinear()
-      .domain([0, 5]) // input
-      .range([height, 50]); // output
+      .domain([y_min - 0.1 * Math.abs(y_max - y_min), y_max])
+      .range([height, 50]);
 
     let linePath = line<DataPoint>()
-      .x(function (d) { return xScale(d.id) as number; }) // set the x values for the line generator
-      .y(function (d) { return yScale(d.value) as number; }); // set the y values for the line generator
+      .x(function (d) { return xScale(d.x) as number; }) // set the x values for the line generator
+      .y(function (d) { return yScale(d.y) as number; }); // set the y values for the line generator
 
 
     svg.append("g")
@@ -56,37 +81,37 @@ const App: React.FC = () => {
       .attr("class", "y axis")
       .attr("transform", "translate(25,-25)")
       .call(axisLeft(yScale));
-    if (data) {
-      svg.select(".line").remove();
-      svg.append("path")
-        .datum(data) // 10. Binds data to the line
-        .attr("class", "line") // Assign a class for styling
-        .attr("d", linePath) // 11. Calls the line generator
-        .attr("fill", "none")
-        .attr("pointer-events", "visibleStroke")
-        .style("stroke", "black")
-        .attr("stroke-width", "1px");
 
-      svg.selectAll(".dot").remove();
-      // 12. Appends a circle for each datapoint
-      svg.selectAll(".dot")
-        .data(data)
-        .enter().append("circle") // Uses the enter().append() method
-        .attr("class", "dot") // Assign a class for styling
-        .attr("cx", function (d) { return xScale(d.id) as number; })
-        .attr("cy", function (d) { return yScale(d.value) as number; })
-        .attr("r", function (d) { return d.size * 5 })
-        .on("mouseover", function (a, b, c) {
-          console.log(a);
-        })
-        .on("mouseout", function () { });
-    }
+    svg.select(".line").remove();
+    svg.append("path")
+      .datum(data) // 10. Binds data to the line
+      .attr("class", "line") // Assign a class for styling
+      .attr("d", linePath) // 11. Calls the line generator
+      .attr("fill", "none")
+      .attr("pointer-events", "visibleStroke")
+      .style("stroke", "black")
+      .attr("stroke-width", "1px");
+
+    svg.selectAll(".dot").remove();
+    // 12. Appends a circle for each datapoint
+    svg.selectAll(".dot")
+      .data(data)
+      .enter().append("circle") // Uses the enter().append() method
+      .attr("class", "dot") // Assign a class for styling
+      .attr("cx", function (d) { return xScale(d.x) as number; })
+      .attr("cy", function (d) { return yScale(d.y) as number; })
+      .attr("r", function (d) { return d.size * 5 })
+      .on("mouseover", function (a) {
+        console.log(a);
+      })
+      .on("mouseout", function () { });
+
   }, [data]);
 
   return (
     <div className="container">
       <svg id="journey-timeline" ref={svgRef} />
-      <button id="refresh-button" onClick={() => mutate('url=https://www.notion.so/c9900066ef9543d986543f7db6594ae3?v=e1498ba37c6043dc9cda20aed703a3a7')}> <img alt="refresh graph" src={refreshIcon} /></button>
+      <button id="refresh-button" onClick={() => mutate(searchLocation)}> <img alt="refresh graph" src={refreshIcon} /></button>
     </div>
   );
 }
